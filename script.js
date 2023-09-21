1b8b9de868e817ef13299e5928465080de2e5ad4
gwadholm <gwadholm@gmail.com>
2023-09-21 11:36:57 -0400

    changed equality and updated calls to divisibleByTwo

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function firstLetterName(name) {
 }
 
 function divisibleByTwo(x) {
-  if (x%2==0) {
+  if (x%2===0) {
     return true;
   } else {
     return false;
@@ -20,15 +20,17 @@ function largestNum(arr)  {
   return largestNum;
 }
 
-firstLetterName("Tom");
-
 function numCheck(x) {
   console.log("The number " + x + " is divisible by 2: " + divisibleByTwo(x));
 }
 
+firstLetterName("Tom");
+
 numCheck(8);
 numCheck(9);
 numCheck(13);
+
+console.log(divisibleByTwo(43));
 
 let arrTest = [2,3,16,8,19,22,5];
 console.log(largestNum(arrTest));
