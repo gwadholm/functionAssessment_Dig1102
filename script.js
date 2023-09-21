b0c27c242123f120f369a007606f534ce55150f0
gwadholm <gwadholm@gmail.com>
2023-09-21 11:30:36 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+function firstLetterName(name) {
+  alert("The name " + name + " starts with the letter " + name[0]);
+}
+
+function divisibleByTwo(x) {
+  if (x%2==0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function largestNum(arr)  {
+  let largestNum = arr[0];
+  for (i = 0; i <= arr.length - 1; i++) {
+    if (arr[i] > largestNum) {
+      largestNum = arr[i];
+    }    
+  }
+  return largestNum;
+}
+
+firstLetterName("Tom");
+
+function numCheck(x) {
+  console.log("The number " + x + " is divisible by 2: " + divisibleByTwo(x));
+}
+
+numCheck(8);
+numCheck(9);
+numCheck(13);
+
+let arrTest = [2,3,16,8,19,22,5];
+console.log(largestNum(arrTest));
+
